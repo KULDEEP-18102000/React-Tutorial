cda5ba659ac6cf0f03552dbd090d880046b3be0c
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-04-13 20:11:56 +0530

    styling and used js in jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,22 @@
-function ExpenseItem(){
-    return(
-        <div>
-        <h2>Expense Item</h2>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
-        </div>
-    )
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const LocationOfExpenditure="gwalior"
+
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+        
+      </div>
+      <div >{LocationOfExpenditure}</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
